4c807d835b7f7266649673212c173f178eab9492
Svygzhryr <pamixy@gmail.com>
2023-05-15 14:55:13 +0300

    feat: timer add

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,14 +1,36 @@
-let app = document.createElement('section');
-app.className = 'app';
+let app = document.createElement("section");
+app.className = "app";
 document.body.appendChild(app);
 
-let grid = document.createElement('div');
-grid.className = 'grid'
+let controls = document.createElement('div');
+controls.className = 'controls';
+app.appendChild(controls);
+
+let clock = document.createElement("div");
+clock.className = "clock";
+clock.innerHTML = "00:00";
+controls.appendChild(clock);
+
+let grid = document.createElement("div");
+grid.className = "grid";
 app.appendChild(grid);
+
+let updateClock = async function() {
+  let digit = 0;
+  await setInterval(() => {
+    digit++;
+    let minutes = Math.floor(digit / 60);
+    let seconds = Math.round(digit % 60);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    clock.innerHTML = `${minutes}:${seconds}`
+  }, 1000)
+}
 
 let generateCells = function() {
   for (let i = 0; i < 100; i++) {
-      let cell = document.createElement('button');
+      let cell = document.createElement("button");
       cell.className = `grid__cell cell${i}`;
       let x = (i % 10) + 1;
       let y = Math.floor(i / 10) + 1;
@@ -27,34 +49,34 @@ let setBombs = function() {
     let randomPos = Math.round(Math.random() * 99);
     let cell = document.querySelector(`.cell${randomPos}`)
     console.log(randomPos);
-    if (cell.classList.contains('bomb')) {
+    if (cell.classList.contains("bomb")) {
       bombs++;
     } else {
-      cell.classList.add('bomb');
-      cell.innerHTML = '*';
+      cell.classList.add("bomb");
+      cell.innerHTML = "*";
     }
   }
 }
 
 let handleCellDown = function(e) {
-    if (e.target.className === 'grid') {
+    if (e.target.className === "grid") {
         return null
     }
     let a = e.target;
-    if (a.classList.contains('grid__cell_active')) {
-      a.classList.remove('grid__cell_active');
-    } a.classList.add('grid__cell_active');
+    if (a.classList.contains("grid__cell_active")) {
+      a.classList.remove("grid__cell_active");
+    } a.classList.add("grid__cell_active");
 }
 
 let handleCellUp = function(e) {
     let a = e.target;
-    if (a.classList.contains('grid__cell_active')) {
-      a.classList.remove('grid__cell_active');
-      a.classList.add('grid__cell_disabled')
-      if (a.classList.contains('bomb')) {
-        console.log('boom')
-        document.querySelectorAll('.bomb').forEach(e => {
-          e.classList.add('exposed');
+    if (a.classList.contains("grid__cell_active")) {
+      a.classList.remove("grid__cell_active");
+      a.classList.add("grid__cell_disabled")
+      if (a.classList.contains("bomb")) {
+        console.log("boom")
+        document.querySelectorAll(".bomb").forEach(e => {
+          e.classList.add("exposed");
         })
         // все события происходящие после проигрыша
       }
@@ -62,14 +84,16 @@ let handleCellUp = function(e) {
 }
 
 let handleCellLeave = function(e) {
-    e.target.classList.remove('grid__cell_active');
+    e.target.classList.remove("grid__cell_active");
 }
 
 generateCells();
+// эту функцию можно вызывать после первого клика
 setBombs();
+updateClock();
 
-grid.addEventListener('contextmenu', (e) => {e.preventDefault()});
-grid.addEventListener('mousedown', handleCellDown);
-grid.addEventListener('mouseup', handleCellUp);
-grid.addEventListener('mouseout', handleCellLeave);
+grid.addEventListener("contextmenu", (e) => {e.preventDefault()});
+grid.addEventListener("mousedown", handleCellDown);
+grid.addEventListener("mouseup", handleCellUp);
+grid.addEventListener("mouseout", handleCellLeave);
 

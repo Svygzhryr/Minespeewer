9f02433c177f884cee8d45a67ec743348fa289ca
Svygzhryr <pamixy@gmail.com>
2024-06-05 16:49:38 +0300

    refactor: var names change and neighbor check implementation

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,16 +1,19 @@
-let clicks = 0;
-let stopTimer = false;
-let bombs = 10;
-let cells = 100;
-let checked = 1;
-let firstMove = true;
+const bombs = 10;
+const cells = 100;
+
+let clickCount = 0;
+let cellsChecked = 1;
+let isTimerStopped = false;
+let isFirstMove = true;
+
+let timer;
 
 let app = document.createElement("section");
 app.className = "app";
 document.body.appendChild(app);
 
-let controls = document.createElement('div');
-controls.className = 'controls';
+let controls = document.createElement("div");
+controls.className = "controls";
 app.appendChild(controls);
 
 let clock = document.createElement("div");
@@ -22,34 +25,33 @@ let grid = document.createElement("div");
 grid.className = "grid";
 app.appendChild(grid);
 
-let newGame = document.createElement('button');
+let newGame = document.createElement("button");
 newGame.className = "restart restart_alt";
-newGame.innerHTML = "Reset"
-controls.appendChild(newGame)
+newGame.innerHTML = "Reset";
+controls.appendChild(newGame);
 
-let end = document.createElement('div');
-let restart = document.createElement('button');
-let endMessage = document.createElement('h2');
-endMessage.className = 'end__message';
-endMessage.innerHTML = `Game over in ${clicks} moves. Try again.`;
-end.className = 'end';
-restart.className = 'restart';
-restart.innerHTML = 'Restart'
+let end = document.createElement("div");
+let restart = document.createElement("button");
+let endMessage = document.createElement("h2");
+endMessage.className = "end__message";
+endMessage.innerHTML = `Game over in ${clickCount} moves. Try again.`;
+end.className = "end";
+restart.className = "restart";
+restart.innerHTML = "Restart";
 app.appendChild(end);
 end.appendChild(endMessage);
 end.appendChild(restart);
 
-let mask = document.createElement('div');
+let mask = document.createElement("div");
 mask.className = "mask";
 end.appendChild(mask);
 
-let timer;
-let updateClock = async function() {
+let updateClock = async function () {
   let digit = 0;
-  timer = await setInterval(() => {
-    if (stopTimer) {
+  timer = setInterval(() => {
+    if (isTimerStopped) {
       clearInterval(timer);
-      return
+      return;
     }
     digit++;
     let minutes = Math.floor(digit / 60);
@@ -57,29 +59,42 @@ let updateClock = async function() {
 
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
-    clock.innerHTML = `${minutes}:${seconds}`
-  }, 1000)
-}
+    clock.innerHTML = `${minutes}:${seconds}`;
+  }, 1000);
+};
 
-let generateCells = function() {
+let generateCells = function () {
   for (let i = 0; i < cells; i++) {
-      let cell = document.createElement("button");
-      cell.className = `grid__cell cell${i}`;
-      let x = (i % 10) + 1;
-      let y = Math.floor(i / 10) + 1;
-      cell.setAttribute(`data-x`, x);
-      cell.setAttribute(`data-y`, y);
-    //   cell.innerHTML = `${x} </br> ${y}`;
-      grid.appendChild(cell);
+    let cell = document.createElement("button");
+    cell.className = `grid__cell cell${i}`;
+    let x = (i % 10) + 1;
+    let y = Math.floor(i / 10) + 1;
+    cell.setAttribute(`data-x`, x);
+    cell.setAttribute(`data-y`, y);
+    grid.appendChild(cell);
   }
+};
 
-}
+let checkNeighbors = (x, y) => {
+  // проверка всех клеток по часовой начиная от верхней левой
+  // NorthWest, North, NorthEast, East, SouthEast, South, SouthWest, West
+  let nw = document.querySelector(`[data-x="${x - 1}"][data-y="${y - 1}"]`);
+  let n = document.querySelector(`[data-x="${x}"][data-y="${y - 1}"]`);
+  let ne = document.querySelector(`[data-x="${x + 1}"][data-y="${y - 1}"]`);
+  let e = document.querySelector(`[data-x="${x + 1}"][data-y="${y}"]`);
+  let se = document.querySelector(`[data-x="${x + 1}"][data-y="${y + 1}"]`);
+  let s = document.querySelector(`[data-x="${x}"][data-y="${y + 1}"]`);
+  let sw = document.querySelector(`[data-x="${x - 1}"][data-y="${y + 1}"]`);
+  let w = document.querySelector(`[data-x="${x - 1}"][data-y="${y}"]`);
 
-let setBombs = function(e) {
+  return [nw, n, ne, e, se, s, sw, w];
+};
+
+let setBombs = function (e) {
   let sbombs = bombs;
   for (sbombs; sbombs > 0; sbombs--) {
     let randomPos = Math.round(Math.random() * 99);
-    let cell = document.querySelector(`.cell${randomPos}`)
+    let cell = document.querySelector(`.cell${randomPos}`);
     if (cell.classList.contains("bomb") || cell == e.target) {
       sbombs++;
     } else {
@@ -88,197 +103,136 @@ let setBombs = function(e) {
   }
 
   let bombCells = document.querySelectorAll(".bomb");
-  bombCells.forEach(cell => {
+  bombCells.forEach((cell) => {
     xcord = +cell.dataset.x;
     ycord = +cell.dataset.y;
-    
-    // проверка всех клеток по часовой начиная от верхней левой
-    // NorthWest, North, NorthEast, East, SouthEast, South, SouthWest, West
-    let nw = document.querySelector(`[data-x="${xcord-1}"][data-y="${ycord-1}"]`);
-    let n = document.querySelector(`[data-x="${xcord}"][data-y="${ycord-1}"]`);
-    let ne = document.querySelector(`[data-x="${xcord+1}"][data-y="${ycord-1}"]`);
-    let e = document.querySelector(`[data-x="${xcord+1}"][data-y="${ycord}"]`);
-    let se = document.querySelector(`[data-x="${xcord+1}"][data-y="${ycord+1}"]`);
-    let s = document.querySelector(`[data-x="${xcord}"][data-y="${ycord+1}"]`);
-    let sw = document.querySelector(`[data-x="${xcord-1}"][data-y="${ycord+1}"]`);
-    let w = document.querySelector(`[data-x="${xcord-1}"][data-y="${ycord}"]`);
-  
-    if (nw && !nw.classList.contains("bomb")) {
-      nw.innerHTML = +nw.innerHTML + 1;
 
-    }
-    if (n && !n.classList.contains("bomb")) {
-      n.innerHTML = +n.innerHTML + 1;
+    const neighbors = checkNeighbors(xcord, ycord);
 
-    }
-    if (ne && !ne.classList.contains("bomb")) {
-      ne.innerHTML = +ne.innerHTML + 1;
+    neighbors.map((cell) => {
+      if (cell && !cell.classList.contains("bomb")) ++cell.innerHTML;
+    });
+  });
+};
 
-    }
-    if (e && !e.classList.contains("bomb")) {
-      e.innerHTML = +e.innerHTML + 1;
-
-    }
-    if (se && !se.classList.contains("bomb")) {
-      se.innerHTML= +se.innerHTML + 1;
-
-    }
-    if (s && !s.classList.contains("bomb")) {
-      s.innerHTML = +s.innerHTML + 1;
-
-    }
-    if (sw && !sw.classList.contains("bomb")) {
-      sw.innerHTML = +sw.innerHTML + 1;
-
-    }
-    if (w && !w.classList.contains("bomb")) {
-      w.innerHTML = +w.innerHTML + 1;
-
-    }
-    
-  })
-}
-
-let resetCells = function() {
-  document.querySelectorAll('.grid__cell').forEach(e => {
+let resetCells = function () {
+  document.querySelectorAll(".grid__cell").forEach((e) => {
     e.remove();
   });
   clock.innerHTML = "00:00";
-  stopTimer = false;
-  clicks = 0;
+  isTimerStopped = false;
+  clickCount = 0;
   startGame();
-}
+};
 
-let checkNeighbors = function(el) {
-  let currentCell = el.target;
+let handleCellDown = function (e) {
+  if (e.target.className === "grid") {
+    return null;
+  }
+  let a = e.target;
+  if (a.classList.contains("grid__cell_active")) {
+    a.classList.remove("grid__cell_active");
+  }
+  a.classList.add("grid__cell_active");
+};
 
-  // let check = function() {
-   
-  //   let xcord = +currentCell.dataset.x;
-  //   let ycord = +currentCell.dataset.y;
-   
-  //   let n = document.querySelector(`[data-x="${xcord}"][data-y="${ycord-1}"]`);
-  //   let e = document.querySelector(`[data-x="${xcord+1}"][data-y="${ycord}"]`);
-  //   let s = document.querySelector(`[data-x="${xcord}"][data-y="${ycord+1}"]`);
-  //   let w = document.querySelector(`[data-x="${xcord-1}"][data-y="${ycord}"]`);
+let handleCellUp = function (e) {
+  // проверка пкм ли это
+  if (e.button == 2) {
+    return;
+  }
 
-  //   if (!n || !e || !s || !w) return;  
-  //   if (currentCell.innerHTML == 0) {
-  //     currentCell.classList.add("grid__cell_disabled")
-  //     switch (true) {
-  //       case (n.innerHTML == 0):
-  //         check(currentCell = n)
-  //         break
-  //       case (e.innerHTML == 0):
-  //         check(currentCell = e)
-  //       case (s.innerHTML == 0):
-  //         check(currentCell = s)
-  //         break
-  //       case (w.innerHTML == 0):
-  //         check(currentCell = w)
-  //     }
-  //   }
-  // }
+  let a = e.target;
+  // проверка на флажок
+  if (a.classList.contains("flagged")) {
+    return;
+  }
 
-  // check(el);
-}
-
-let handleCellDown = function(e) {
-    if (e.target.className === "grid") {
-        return null
-    }
-    let a = e.target;
-    if (a.classList.contains("grid__cell_active")) {
-      a.classList.remove("grid__cell_active");
-    } a.classList.add("grid__cell_active");
-}
-
-let handleCellUp = function(e) {
-    // проверка пкм ли это
-    if (e.button == 2) {return}
-    let a = e.target;
-    // проверка на флажок
-    if (a.classList.contains("flagged")) {return}
-    if (a.classList.contains("grid__cell_active")) {
-      if (!a.classList.contains("grid__cell_disabled")) {
-        // первый ли это ход
-        if (firstMove) {
-          firstMove = false;
-          setBombs(e);
-          updateClock();
-
-        }
-
-        // окрашивание и появление текста в нажатой клетке в зависимости от количества ближайших бомб
-        if (!e.target.classList.contains("bomb")) {
-          let bn = e.target.innerHTML;
-          e.target.style.color = `rgb(${bn*31}, 60, ${255 - bn*31})`;
-          checkNeighbors(e);
-        }
-
-        clicks++;
-        // проверка на бомбу нажатой клетки
-        if (a.classList.contains("bomb")) {
-          document.querySelectorAll(".bomb").forEach(e => {
-            e.innerHTML = "*";
-
-            e.classList.add("exposed");
-          })
-
-          // все события происходящие после проигрыша
-          let moveString = clicks % 10 == 1 && clicks !== 11 ? 'move' : 'moves';
-          endMessage.innerHTML = `Game over in ${clicks} ${moveString}. Try again.`;
-          stopTimer = true;
-          setTimeout(() => {
-            end.classList.add('end_active');
-          }, 1500)
-        }
-        checked = 1;
-        document.querySelectorAll('.grid__cell_disabled').forEach(e => {
-          checked++;
-        })
-        if (checked == cells - bombs) {
-          document.querySelectorAll(".bomb").forEach(e => {
-            e.innerHTML = "*";
-            e.classList.add("exposed");
-          })
-          // события после выигрыша
-          let moveString = clicks % 10 == 1 && clicks !== 11 ? 'move' : 'moves';
-          endMessage.innerHTML = `Congrats! You won in ${clicks} ${moveString}.`;
-          stopTimer = true;
-          setTimeout(() => {
-            end.classList.add('end_active');
-          }, 1500)
-        }
+  if (a.classList.contains("grid__cell_active")) {
+    if (!a.classList.contains("grid__cell_disabled")) {
+      // первый ли это ход
+      if (isFirstMove) {
+        isFirstMove = false;
+        setBombs(e);
+        updateClock();
       }
-      a.classList.remove("grid__cell_active");
-      a.classList.add("grid__cell_disabled");
-    } 
-}
 
-let handleRestart = function() {
+      // окрашивание и появление текста в нажатой клетке в зависимости от количества ближайших бомб
+      if (!e.target.classList.contains("bomb")) {
+        let bn = e.target.innerHTML;
+        e.target.style.color = `rgb(${bn * 31}, 60, ${255 - bn * 31})`;
+      }
+
+      clickCount++;
+      // проверка на бомбу нажатой клетки
+      if (a.classList.contains("bomb")) {
+        document.querySelectorAll(".bomb").forEach((e) => {
+          e.innerHTML = "*";
+
+          e.classList.add("exposed");
+        });
+
+        // все события происходящие после проигрыша
+        let moveString =
+          clickCount % 10 == 1 && clickCount !== 11 ? "move" : "moves";
+        endMessage.innerHTML = `Game over in ${clickCount} ${moveString}. Try again.`;
+        isTimerStopped = true;
+        setTimeout(() => {
+          end.classList.add("end_active");
+        }, 1500);
+      }
+
+      cellsChecked = 1;
+      document.querySelectorAll(".grid__cell_disabled").forEach((e) => {
+        cellsChecked++;
+      });
+
+      if (cellsChecked == cells - bombs) {
+        document.querySelectorAll(".bomb").forEach((e) => {
+          e.innerHTML = "*";
+          e.classList.add("exposed");
+        });
+        // события после выигрыша
+        let moveString =
+          clickCount % 10 == 1 && clickCount !== 11 ? "move" : "moves";
+        endMessage.innerHTML = `Congrats! You won in ${clickCount} ${moveString}.`;
+        isTimerStopped = true;
+        setTimeout(() => {
+          end.classList.add("end_active");
+        }, 1500);
+      }
+    }
+    a.classList.remove("grid__cell_active");
+    a.classList.add("grid__cell_disabled");
+  }
+};
+
+let handleRestart = function () {
   clearInterval(timer);
-  firstMove = true;
+  isFirstMove = true;
   resetCells();
-  end.classList.remove('end_active');
-}
+  end.classList.remove("end_active");
+};
 
-let handleCellLeave = function(e) {
+let handleCellLeave = function (e) {
   e.target.classList.remove("grid__cell_active");
-}
+};
 
-let handleRMB = function(e) {
+let handleRMB = function (e) {
   let a = e.target;
   e.preventDefault();
-  if (!a.classList.contains("grid__cell_disabled") && a.classList.contains("grid__cell")) {
+  if (
+    !a.classList.contains("grid__cell_disabled") &&
+    a.classList.contains("grid__cell")
+  ) {
     a.classList.toggle("flagged");
   }
-  return false
-}
+  return false;
+};
 
-let startGame = function() {
+let startGame = function () {
   generateCells();
-}
+};
 
 startGame();
 
@@ -288,4 +242,3 @@ grid.addEventListener("mouseup", handleCellUp);
 grid.addEventListener("mouseout", handleCellLeave);
 restart.addEventListener("click", handleRestart);
 newGame.addEventListener("click", handleRestart);
-
